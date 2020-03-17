407d9e791998bc96c38d3cfcb39d7d18bf24b51b
Tony Mowers <tony@amowers.com>
2020-03-17 03:18:34 +0100

    #9 add javascript build to maven

---
+++ src/frontend/index.ts
@@ -0,0 +1,35 @@
+import * as _ from 'lodash';
+import './styles/main.scss';
+import Icon from './assets/house.svg';
+
+function createGreeting() {
+    const element: HTMLElement = document.createElement('span');
+    element.innerHTML = _.join([new Greeter().greet(), 'I wish you a very good day.'], ' ');
+    return element;
+}
+
+function createImage() {
+    const myIcon: HTMLImageElement = new Image(400, 400);
+    myIcon.src = Icon;
+    return myIcon;
+}
+
+function component() {
+    const result: HTMLElement = document.createElement("div");
+    result.appendChild(createGreeting());
+    result.appendChild(createImage());
+
+    return result;
+}
+
+
+
+class Greeter {
+    private greeting: string = "Hello world!";
+
+    greet() {
+        return this.greeting;
+    }
+}
+
+document.body.appendChild(component());
