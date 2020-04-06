3593f8afa839f3afc0cdfed1910e046be9371e57
Tony Mowers <tony@amowers.com>
2020-04-06 18:50:50 +0200

    remove unused javascript files

--- src/frontend/tests/mocha.js
+++
@@ -1,9 +0,0 @@
-var common = [
-    '--require-module tsconfig-paths/register',
-    '--require-module ts-node/register',
-    '  src/fontend/tests/**/*.test.ts'
-].join(' ')
-
-module.exports = {
-    default: common,
-}
